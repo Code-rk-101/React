e81ee87800591b2d454f5b89bb41ed9cf27274c5
Code-rk-101 <meetritik.kr@gmail.com>
2026-01-11 11:12:22 +0530

    Update fragments list component

--- 18_Fragments/Fragments/src/components/list.jsx
+++ 18_Fragments/Fragments/src/components/list.jsx
@@ -4,7 +4,7 @@ function List ({Items})
     // .map is used to convert array data into jsx item
     return (
         <ul className ="list-group">
-            {Items.map((item) => 
+            {Items.map((item) =>//maps are used as loop in rects or convert array to componets 
             (
                 <ListItem itemList = {item}></ListItem>
             ))}
